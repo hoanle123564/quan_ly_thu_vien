9db122ec75d0c2ccdc2dbe2a47bd9a64c9d793f2
hoanle123564 <prysma123564@gmail.com>
2025-12-05 11:40:27 +0700

    format code

--- src/config/connectdb1.js
+++ src/config/connectdb1.js
@@ -1,17 +1,18 @@
 // config/connectdb.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/QuanLyMuonSach', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected via Mongoose');
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27017/QuanLyMuonSach", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected via Mongoose");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
