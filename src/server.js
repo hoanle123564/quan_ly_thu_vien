04de35367f2b66156ebe72e4abfe0dea3b5383e2
hoanle123564 <prysma123564@gmail.com>
2025-12-05 11:42:36 +0700

    format code

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const ViewEngine = require('./config/viewEngine')
-const WebRoute = require('./routes/web')
-const bodyParser = require('body-parser')
-const connectDB = require('./config/connectdb1');
-const cors = require('cors');
-const port = 3000
+const express = require("express");
+const ViewEngine = require("./config/viewEngine");
+const WebRoute = require("./routes/web");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/connectdb1");
+const cors = require("cors");
+const port = 3000;
 
-let app = express()
+let app = express();
 
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 // app.use(express.json)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// Tăng giới hạn kích thước payload
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 connectDB();
 
-ViewEngine(app)
-app.use('/', WebRoute)
+ViewEngine(app);
+app.use("/", WebRoute);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
